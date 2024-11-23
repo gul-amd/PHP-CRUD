0a915f9256650c60be843992a3da6f4d4b414b0c
Gulamo <gulamo.amd@gmail.com>
2024-11-23 17:49:43 +0200

    buil: php crud app

---
+++ dbcon.php
@@ -0,0 +1,19 @@
+<?php
+
+    define("HOSTNAME", "localhost");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    define("DATABASE", "operations");
+
+    $connection = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+
+    if(!$connection){
+        die("connection Faild");
+    }
+    
+    else{
+        echo "conected";
+    }
+
+
+?>
---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+        crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="style.css">    
+
+</head>
+<body>
+    <h1 id="main_title">Clientes</h1>
+
+    <div class="container">
+        <h2>Lista Completa</h2>
+        <table class="table table-hover table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Spbrenome</th>
+                    <th>Idade</th>
+                </tr>
+            </thead>
+            <tbody>
+                    <th>01</th>
+                    <th>Jose</th>
+                    <th>Antonio</th>
+                    <th>22</th>
+                </tr>
+                    <th>02</th>
+                    <th>Domingos</th>
+                    <th>Jorge</th>
+                    <th>28</th>
+                </tr>
+                    <th>03</th>
+                    <th>Castigo</th>
+                    <th>Luiz</th>
+                    <th>25</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+        crossorigin="anonymous">
+    </script>
+
+</body>
+</html>
